e24cdc8b36126fa87ac64811e0496eeda29e5704
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2019-10-24 16:52:36 +0100

    development update

--- models/User.js
+++ models/User.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
+
+const creditSchema = mongoose.Schema({
+  
+})
+
+
 const userSchema = mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
