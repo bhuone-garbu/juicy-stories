e2f2eb87530a00bbf23ff3525b19b9865e6b220e
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2019-10-24 17:29:29 +0100

    express router setup

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/juicy-contents'
-const secret = process.env.SECRET || 's0m3 s3cr3t'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/juicy-stories'
+const secret = process.env.SECRET || 's0m3 s3cr3t' // jsonwebtoken
 
 module.exports = { port, dbURI, secret }
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const app = require('express')()
-const port = process.env.PORT || 4000
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { dbURI, port } = require('./config/environment')
+const router = require('./config/router')
+
+// connect mongoose
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => console.log('MongoDB connected!!!'))
+
 
 // hooking up the body parser so that request are parsed into JSON
 app.use(bodyParser.json())
+
+// handle all the GET, POST, PUT related http requests
+app.use('/api', router)
+
+// this is for anything that the router did not response on
+app.get('/*', (req, res) => res.status(404).json({ message: 'not found' }))
 
 app.listen(port, () => console.log(`Express server now running and listening to port ${port}`))
