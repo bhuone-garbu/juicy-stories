2a50dae2f7b6aed22eff556911fd182edf4e141b
mElbo <mortenelbo01@gmail.com>
2019-10-28 16:05:07 +0000

    fixing merge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,31 +39,6 @@ class Navbar extends React.Component {
     console.log(this.props.hello)
     const authCheck = this.state.isAuthenticated
     return (
-<<<<<<< HEAD
-      <header className="navbar md-padding">
-        <section className="navbar-section">
-          <a href="..." className="navbar-brand mr-4">
-            <img className="img-responsive" src={logo} width="100px" alt="logo" />
-          </a>
-          <a href="..." className="btn btn-link">About</a>
-        </section>
-        <section className="navbar-center">
-          <div className="input-group input-inline">
-            <select>
-              <option>Video</option>
-              <option>Audio</option>
-            </select>
-            <input className="form-input" type="text" placeholder="search for juicy stories" />
-            <button className="btn bg-dark input-group-btn">Search</button>
-          </div>
-        </section>
-        <section className="navbar-section">
-          {!authCheck && <Link to="/login"><button className="btn bg-warning input-group-btn">Login</button></Link>}
-          {authCheck && <button type="submit" className="btn bg-warning input-group-btn" onClick={this.handleLogout}>Logout</button>}
-          <Link to="/register"><button className="btn bg-error input-group-btn">Sign Up</button></Link>
-        </section>
-      </header>
-=======
       <div className="bg-primary">
         <div className="container">
           <header className="navbar md-padding">
@@ -91,7 +66,6 @@ class Navbar extends React.Component {
           </header>
         </div>
       </div>
->>>>>>> development
     )
   }
 }
