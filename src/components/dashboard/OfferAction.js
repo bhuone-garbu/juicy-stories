0342fe6a2239f6c4f3945e30df42cac462eab0b7
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2019-10-31 18:24:19 +0000

    bugfixes

--- src/components/dashboard/OfferAction.js
+++ src/components/dashboard/OfferAction.js
@@ -1,35 +1,42 @@
 import React from 'react'
-// import { Link } from 'react-routegcor-dom'
 
+import OfferActionModal from '../modals/OfferActionModal'
+
+// this is component will be responsible to handle the action to accept or reject on an open offer
 class OfferAction extends React.Component {
 
   constructor() {
     super()
     this.state = {
-      confirmAccept: false,
-      confirmReject: false
+      option: null
     }
+
+    this.handleAction = this.handleAction.bind(this)
   }
 
-  // handle the 
+  
+  // handle the confirmation to accept to decline the offer
   handleAction({ target: { name } }){
-    this.setState({ ...this.state, [name]: true })
+    this.setState({ option: name })
   }
 
 
   render() {
-    const { offer } = this.props
+    const { offer, toggleMessage, handleConfirm } = this.props
     return (
       <>
-        <p>Received offer: <span className="text-bold h4">{offer.offerPrice} JC</span></p>
+        <OfferActionModal offer={offer} story={offer.story}/>
+        <p className="text-center">Received offer: <span className="text-bold h4">{offer.offerPrice} JC</span></p>
         <div className="h-center">
-          <button className="btn bg-success input-group-btn" name="accept">Accept</button>
-          <button className="btn bg-primary input-group-btn" name="reject">Reject</button>
-          <button className="btn bg-warning input-group-btn" name="counter">Counter</button>
+          <a href="#offerActionModal">
+            <button className="btn bg-success input-group-btn" type="button" onClick={handleConfirm.bind(this,offer,'accept')} value="accept">Accept</button>
+            <button className="btn bg-primary input-group-btn" type="button" onClick={handleConfirm.bind(this,offer,'reject')} value="reject">Reject</button>
+          </a>
+          {/* <button className="btn bg-warning input-group-btn" name="counter">Counter</button> */}
         </div>
         <div className="vertical-margin h-padding v-margin h-center">
           
-          <button onClick={this.props.handleClick}>
+          <button className="btn bg-gray tooltip tooltip-bottom v-center" data-tooltip="Show/Hide messages" onClick={toggleMessage}>
             <i className="icon icon-2x icon-mail"/>Messages
           </button>
           
